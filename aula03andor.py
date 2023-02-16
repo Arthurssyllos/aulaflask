feb07991fb93fa2cef8f6d5cea2462689635e504
EC2 Default User <ec2-user@ip-172-31-25-180.ec2.internal>
2023-02-16 01:05:34 +0000

    Aula And e Or.

--- aula03andor.py
+++ aula03andor.py
@@ -1,6 +1,6 @@
 idade = int(input("Digite a sua idade: "))
 dinheiro = int(input("Digite a quantidade de dinheiro que você tem: "))
-carteira = (input("Você tem carteira de motorista? (s/n)")
+carteira = input("Você tem carteira de motorista? (S/N): ")
 
 if (idade >= 18 and dinheiro >= 50) or carteira == "s":
     print("Você pode alugar o carro.")
